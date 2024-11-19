03ea25da51566715a4395975aafd0959384ef76b
Alex Sandri <me@alexsandri.com>
2024-11-19 12:18:51 +0100

    create models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Lega(models.Model):
+    nome = models.TextField()
+
+
+class Sport(models.Model):
+    nome = models.TextField()
+
+
+class Specialità(models.Model):
+    nome = models.TextField()
+    sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
+
+
+class Squadra(models.Model):
+    compagnia = models.PositiveSmallIntegerField()
+    lega = models.ForeignKey(Lega, on_delete=models.CASCADE)
+    sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
