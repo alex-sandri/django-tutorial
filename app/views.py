e81cc496abe4f14e2cadab0a4b5f6a6c2ff176cc
Alex Sandri <me@alexsandri.com>
2024-11-20 11:48:56 +0100

    define templates

--- app/views.py
+++ app/views.py
@@ -1,4 +1,15 @@
 from django.http import HttpResponse
+from django.template import loader
+from .models import Sport
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    sports = Sport.objects.all()
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({'sports': sports}))
+
+
+def sport(request, sport_id):
+    sport = Sport.objects.get(pk=sport_id)
+    template = loader.get_template('sport.html')
+    return HttpResponse(template.render({'sport': sport}))
