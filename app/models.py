ac95dba3e42c89026f73574ec4ced0e17d969469
Alex Sandri <me@alexsandri.com>
2024-11-20 11:48:43 +0100

    add __str__ to models

--- app/models.py
+++ app/models.py
@@ -4,17 +4,29 @@ from django.db import models
 class Lega(models.Model):
     nome = models.TextField()
 
+    def __str__(self) -> str:
+        return self.nome
+
 
 class Sport(models.Model):
     nome = models.TextField()
+
+    def __str__(self) -> str:
+        return self.nome
 
 
 class Specialità(models.Model):
     nome = models.TextField()
     sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
 
+    def __str__(self) -> str:
+        return f'{self.nome} ({self.sport})'
+
 
 class Squadra(models.Model):
     compagnia = models.PositiveSmallIntegerField()
     lega = models.ForeignKey(Lega, on_delete=models.CASCADE)
     sport = models.ForeignKey(Sport, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'{self.compagnia} - {self.lega} ({self.sport})'
